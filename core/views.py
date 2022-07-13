72d52db603a0a0cdbc45efa0256943649143b79a
Harsh Mishra <hmisraji07@gmail.com>
2022-07-13 12:32:10 +0530

    Added signin and signout functionality

--- core/views.py
+++ core/views.py
@@ -2,7 +2,10 @@ from click import password_option
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -41,3 +44,24 @@ def signup(request):
             messages.info(request, 'Passwords do not match')
             return redirect('signup')     
     return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('signin')
+        
+    return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def signout(request):
+    logout(request)
+    return redirect('signin')
+
