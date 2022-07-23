e9fe6d7e0b9c9c889da08ba763259d4ac9a6a5c1
Harsh Mishra <hmisraji07@gmail.com>
2022-07-23 11:14:15 +0530

    Added a Posts Model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
-from distutils.command.upload import upload
-from email.policy import default
+from datetime import datetime
+import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model() #prefer over django.contrib.auth.models.User to add custom Authentication models for AUTH_USER_MODEL
@@ -14,4 +14,13 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'post-images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.user
